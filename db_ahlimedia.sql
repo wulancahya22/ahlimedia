02e664c6b76958660a1bd85ee900bdce16216bae
sitianisa28 <sa0348185@gmail.com>
2020-10-12 11:32:22 +0700

    db

--- db_ahlimedia.sql
+++ db_ahlimedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2020 at 04:35 AM
+-- Generation Time: Oct 12, 2020 at 06:27 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -31,6 +31,13 @@ CREATE TABLE `tb_akses` (
   `keterangan` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tb_akses`
+--
+
+INSERT INTO `tb_akses` (`id_akses`, `keterangan`) VALUES
+(1, 'admin');
+
 -- --------------------------------------------------------
 
 --
@@ -39,13 +46,21 @@ CREATE TABLE `tb_akses` (
 
 CREATE TABLE `tb_buku` (
   `id_buku` int(11) NOT NULL,
-  `id_kategori` int(11) NOT NULL,
-  `judul` varchar(50) NOT NULL,
+  `judul` varchar(30) NOT NULL,
   `penulis` varchar(30) NOT NULL,
   `sinopsis` varchar(255) NOT NULL,
-  `no_hp` varchar(15) NOT NULL,
-  `gambar` varchar(255) NOT NULL
+  `nama_file` varchar(50) NOT NULL,
+  `kategori` varchar(20) NOT NULL COMMENT '1:Anak, 2:Dewasa, 3:Karya Ilmiah',
+  `dokumen` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_buku`
+--
+
+INSERT INTO `tb_buku` (`id_buku`, `judul`, `penulis`, `sinopsis`, `nama_file`, `kategori`, `dokumen`) VALUES
+(4, 'qqwee', 'vrfjkvr', 'rkfrujf', 'file_1602299979.jpg', '1', 'file_1602299979.jpg'),
+(5, 'ksaue', 'wghjw', 'kwhkjw', 'file_1602300101.jpg', '2', '');
 
 -- --------------------------------------------------------
 
@@ -58,20 +73,42 @@ CREATE TABLE `tb_kategori` (
   `nama_kategori` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'Anak'),
+(2, 'Dewasa'),
+(3, 'Karya Ilmiah');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_login`
+-- Table structure for table `tb_user`
 --
 
-CREATE TABLE `tb_login` (
-  `id_login` int(11) NOT NULL,
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(20) NOT NULL,
   `id_akses` int(11) NOT NULL,
-  `id_buku` int(11) NOT NULL
+  `jenis_kel` varchar(1) NOT NULL,
+  `tempat_lahir` varchar(20) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `foto` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `id_akses`, `jenis_kel`, `tempat_lahir`, `tanggal_lahir`, `no_hp`, `alamat`, `email`, `foto`) VALUES
+(1, 'Teguh Adi P', 'admin', '123', 1, 'L', 'Malang', '1991-03-05', '085708514574', 'Jl Ki Ageng Gribig gg Kaserin MU no 36 Lesanpuro Kota Malang', 'teguhap05@gmail.com', '');
 
 --
 -- Indexes for dumped tables
@@ -87,8 +124,7 @@ ALTER TABLE `tb_akses`
 -- Indexes for table `tb_buku`
 --
 ALTER TABLE `tb_buku`
-  ADD PRIMARY KEY (`id_buku`),
-  ADD KEY `id_kategori` (`id_kategori`);
+  ADD PRIMARY KEY (`id_buku`);
 
 --
 -- Indexes for table `tb_kategori`
@@ -97,12 +133,11 @@ ALTER TABLE `tb_kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indexes for table `tb_login`
+-- Indexes for table `tb_user`
 --
-ALTER TABLE `tb_login`
-  ADD PRIMARY KEY (`id_login`),
-  ADD KEY `id_akses` (`id_akses`),
-  ADD KEY `id_buku` (`id_buku`);
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_akses` (`id_akses`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -112,38 +147,31 @@ ALTER TABLE `tb_login`
 -- AUTO_INCREMENT for table `tb_akses`
 --
 ALTER TABLE `tb_akses`
-  MODIFY `id_akses` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_akses` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tb_buku`
 --
 ALTER TABLE `tb_buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `tb_login`
+-- AUTO_INCREMENT for table `tb_user`
 --
-ALTER TABLE `tb_login`
-  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `tb_buku`
+-- Constraints for table `tb_user`
 --
-ALTER TABLE `tb_buku`
-  ADD CONSTRAINT `tb_buku_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `tb_kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tb_login`
---
-ALTER TABLE `tb_login`
-  ADD CONSTRAINT `tb_login_ibfk_1` FOREIGN KEY (`id_akses`) REFERENCES `tb_akses` (`id_akses`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tb_login_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `tb_buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_akses`) REFERENCES `tb_akses` (`id_akses`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
